629e38580a2f4e94573e42bc824a615cfe8a4fc8
toran billups <toranb@gmail.com>
2019-08-11 14:33:54 -0500

    DEPENDENCY: update for ember-try/travis

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,73 +1,49 @@
-'use strict';
+/* eslint-env node */
+module.exports = {
+  useYarn: true,
 
-const getChannelURL = require('ember-source-channel-url');
+  command: "ember test -e=production",
 
-module.exports = async function() {
-  return {
-    scenarios: [
-      {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.4.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.8.0'
-          }
-        }
-      },
-      {
-        name: 'ember-release',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('release')
-          }
-        }
-      },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
-      },
-      // The default `.travis.yml` runs this scenario via `npm test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
-      {
-        name: 'ember-default',
-        npm: {
-          devDependencies: {}
-        }
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1'
-          }
+  scenarios: [
+    {
+      name: 'ember-3.4.X',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.4.0'
         }
       }
-    ]
-  };
+    },
+    {
+      name: 'ember-3.5.X',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.5.0'
+        }
+      }
+    },
+    {
+      name: 'ember-3.6.X',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.6.0'
+        }
+      }
+    },
+    {
+      name: 'ember-3.8.X',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.8.0'
+        }
+      }
+    },
+    {
+      name: 'ember-3.11.X',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.11.1'
+        }
+      }
+    }
+  ]
 };
